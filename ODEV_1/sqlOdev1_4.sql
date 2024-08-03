73045e236de97b2bd882c18eaf67c66edc479562
Taha Tayyib Toprak <toprak.tayyib58@gmail.com>
2024-08-03 21:52:00 +0300

    patikaSQL_Odev1Solutiouns

---
+++ ODEV_1/sqlOdev1_4.sql
@@ -0,0 +1,4 @@
+/**customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?**/
+SELECT first_name,last_name FROM customer
+WHERE first_name='Mary';
+/*ANSWER = Smith*/ 
---
+++ ODEV_1/sqlOdev1_5.sql
@@ -0,0 +1,3 @@
+/**film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.***/
+SELECT rental_rate, length FROM film
+WHERE  NOT length>50 AND NOT (rental_rate=2.99 OR rental_rate=4.99);
